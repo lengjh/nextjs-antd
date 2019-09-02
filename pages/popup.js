6ba2514c4383760869762b905a15e40fdc5fc043
Kingwell Leng <jinhua.leng@gmail.com>
2019-09-02 22:07:48 +0800

    feat: 🎸 添加页面
    添加popup页面

--- pages/popup.js
+++ pages/popup.js
@@ -1,67 +1,16 @@
 import React, { Component } from 'react';
 import Head from 'next/head';
 import { Layout, Menu, Breadcrumb, Icon, Row, Col, Button, Input, Card, Empty } from 'antd';
-import css from './index.less';
-import dynamic from 'next/dynamic';
-import MaxView from '../components/MaxView';
-import codeIcon from '../static/code_icon.svg';
-const { Header, Content, Sider, Footer } = Layout;
-const DynamicComponentJsonView = dynamic(() => {
-  return import('../components/JsonView');
-});
-const DynamicComponentCode = dynamic(() => {
-  return import('../components/Code');
-});
-const DynamicComponentQRCode = dynamic(() => {
-  return import('../components/QRCode');
-});
-const DynamicComponentImageBase64 = dynamic(() => {
-  return import('../components/ImageBase64');
-});
-const getCompoent = type => {
-  switch (type) {
-    case '1':
-      return <DynamicComponentJsonView />;
-    case '2':
-      return <DynamicComponentCode />;
-    case '3':
-      return <DynamicComponentQRCode />;
-    case '4':
-      return <DynamicComponentImageBase64 />;
-    default:
-      break;
-  }
-  return <Empty />;
-};
+import css from './popup.less';
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = { type: 1, load: false, collapsed: true };
-    this.setMenu = this.setMenu.bind(this);
   }
 
-  toggle = () => {
-    this.setState({
-      collapsed: !this.state.collapsed,
-    });
-  };
-  componentDidMount() {
-    this.setState({ type: window.location.hash.slice(1) || '1' });
-    setTimeout(() => {
-      this.setState({ load: true });
-    }, 0);
-    window.addEventListener(
-      'hashchange',
-      ev => {
-        this.setState({ type: window.location.hash.slice(1) });
-      },
-      false
-    );
-  }
-  setMenu(ev) {
-    this.setState({ type: ev.key });
-    window.location.hash = ev.key;
-  }
+  componentDidMount() {}
+
   getQueryValue(name) {
     let search = location.search.slice(1);
     let obj = {};
@@ -79,9 +28,11 @@ class App extends Component {
         <Head>
           <title>Home</title>
         </Head>
-        <a href="/html/index.html" target="_blank">
-          index
-        </a>
+        <div className={css.box}>
+          <a href="/html/index.html" target="_blank">
+            index
+          </a>
+        </div>
       </>
     );
   }
